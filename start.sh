a77e26fee9bb16c3dde792ff6f9eb535723a6e7e
root <root@minchulahn@inslab.co.kr>
2017-01-18 13:35:04 +0900

    change start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-/opt/gitlab/embedded/bin/runsvdir-start & gitlab-ctl reconfigure
+/opt/gitlab/embedded/bin/runsvdir-start & 
+gitlab-ctl reconfigure
+
+# Tail all logs
+gitlab-ctl tail &
+# Wait for SIGTERM
+wait
