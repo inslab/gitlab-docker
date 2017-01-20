a63ead689e7c34c39a926800738e7a9825761237
inslab <inslab.dev@gmail.com>
2017-01-20 18:05:29 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-/opt/gitlab/embedded/bin/runsvdir-start & 
+# Copy gitlab.rb for the first time
+if [[ ! -e /etc/gitlab/gitlab.rb ]]; then
+	echo "Installing gitlab.rb config..."
+	sed -i '/^external_url/s|external_url |#external_url |g' /opt/gitlab/etc/gitlab.rb.template
+	sed -i '$a external_url "http://'$DOMAIN'/gitlab"' /opt/gitlab/etc/gitlab.rb.template
+
+	cp /opt/gitlab/etc/gitlab.rb.template /etc/gitlab/gitlab.rb
+	chmod 0600 /etc/gitlab/gitlab.rb
+fi
+
+# Start service manager
+echo "Starting services..."
+/opt/gitlab/embedded/bin/runsvdir-start &
+
+echo "Configuring GitLab..."
 gitlab-ctl reconfigure
 
 # Tail all logs
